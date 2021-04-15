d6e416aef6b61ac5db097569b2ba36eb0305b4b6
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-04-14 20:47:31 -0500

    add comment model, assoc, virtualal

---
+++ models/Comment.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const CommentSchema = new Schema({
+    writtenBy: {
+        type: String
+    },
+    commentBody: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//Create Comment model via Schema
+const Comment = model('Comment', CommentSchema);
+
+//Export the model
+modules.exports = Comment;
